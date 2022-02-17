ac6c1878661cba354dc7984a4cee88b34324e779
Folkert <folkert@folkertdev.nl>
2022-02-17 11:22:06 +0100

    reproduce that turning it off and on again hangs

--- src/bin/hello.rs
+++ src/bin/hello.rs
@@ -5,7 +5,7 @@ use nrf9160_rust_starter as _; // global logger + panicking-behavior + memory la
 
 use nrf9160_hal as hal;
 
-use hal::gpio::{Level, Output, PushPull};
+use hal::gpio::Level;
 use hal::uarte;
 
 use core::fmt::Write;
@@ -14,21 +14,47 @@ use core::fmt::Write;
 fn main() -> ! {
     let p = hal::pac::Peripherals::take().unwrap();
     let pins0 = hal::gpio::p0::Parts::new(p.P0_NS);
-    let uart_pins = hal::uarte::Pins {
+    let pins = hal::uarte::Pins {
         rxd: pins0.p0_05.into_floating_input().degrade(),
         txd: pins0.p0_06.into_push_pull_output(Level::High).degrade(),
         cts: None,
         rts: None,
     };
 
-    let mut uarte = hal::Uarte::new(
-        p.UARTE0_NS,
-        uart_pins,
+    let device = p.UARTE0_NS;
+
+    // init 1
+
+    let mut uart = hal::Uarte::new(
+        device,
+        pins,
         uarte::Parity::EXCLUDED,
         uarte::Baudrate::BAUD115200,
     );
 
-    write!(uarte, "Hello, World!\r\n").unwrap();
+    write!(uart, "write 1\r\n").unwrap();
+    write!(uart, "write 2\r\n").unwrap();
+
+    // deinit 1
+    let (device, pins) = uart.free();
+    device.events_txstopped.reset();
+    device.tasks_stoptx.write(|w| w.tasks_stoptx().trigger());
+
+    while device.events_txstopped.read().bits() == 0 {
+        cortex_m::asm::nop();
+    }
+
+    device.enable.write(|w| w.enable().disabled());
+
+    // init 2
+    let mut uart = hal::Uarte::new(
+        device,
+        pins,
+        uarte::Parity::EXCLUDED,
+        uarte::Baudrate::BAUD115200,
+    );
+
+    write!(uart, "write 3\r\n").unwrap();
 
     nrf9160_rust_starter::exit()
 }
